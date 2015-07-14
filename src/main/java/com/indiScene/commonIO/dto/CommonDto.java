3b6956ec978e6de9dfef8930cdf4623a31f5b86e
perghost1 <perghost@icloud.com>
2015-07-14 17:06:39 +0900

    jungseung 2015.07.14 17:06

---
+++ src/main/java/com/indiScene/commonIO/dto/CommonDto.java
@@ -0,0 +1,19 @@
+package com.indiScene.commonIO.dto;
+
+import java.util.Date;
+
+public interface CommonDto {
+
+	public String getContent();
+	public void setContent(String content);
+
+	public String getFile_path();
+	public void setFile_path(String file_path);
+
+	public String getFile_name();
+	public void setFile_name(String file_name);
+	
+
+	
+	
+}
