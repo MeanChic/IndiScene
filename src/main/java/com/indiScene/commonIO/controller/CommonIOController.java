d5555368a16d271018a56a98f1c85600307d52f0
nhj <nhj@nhj-PC>
2015-07-31 16:57:37 +0900

    common

--- src/main/java/com/indiScene/commonIO/controller/CommonIOController.java
+++ src/main/java/com/indiScene/commonIO/controller/CommonIOController.java
@@ -57,7 +57,7 @@ public class CommonIOController  {
 	@description :멀티파일 다울로드위 위해 service를 호출한다
 	 */
 	@RequestMapping(value="/CommonIO/download.do", method=RequestMethod.GET)
-	public ModelAndView ddownloadownload(HttpServletRequest request, HttpServletResponse response)throws Throwable{
+	public ModelAndView download(HttpServletRequest request, HttpServletResponse response)throws Throwable{
 		logger.info("download------------------------------------");
 		
 		ModelAndView mav = new ModelAndView();
